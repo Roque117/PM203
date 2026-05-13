c88d1720aefca556a9ec8154968c13f0dc0085ff
Roque117 <roquejos321@gmail.com>
2026-05-12 22:37:58 -0600

    Practica1/servidor.js
    Se avanzo en el archivo de servidor

---
+++ Practica1/servidor.js
@@ -0,0 +1,20 @@
+ 
+ console.log("Hola Mundo js Servidor");
+
+console.time("MiProceso");
+
+for(let i=0; i<10000; i++){}
+
+console.timeEnd("MiProceso");
+
+
+
+let usuarios=[
+    {nombre:"Ivan", edad: 38},
+    {nombre:"Isay", edad: 38},
+];
+
+console. table(usuarios);
+
+
+
