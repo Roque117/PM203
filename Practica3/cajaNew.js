686e9958bcca8c6967afffdfa948cdd7349eee78
Roque117 <roquejos321@gmail.com>
2026-05-19 20:04:25 -0600

    carpeta practica 3 y cajaNew.js a sido creada
    Se agrego una nueva carpeta y un nuevo archivo que es una extension de la practica 2 y el programa entero, se agrego una nueva funcin cuya utilidad es el mostrar el IVA, subtotal y total de la compra.

---
+++ Practica3/cajaNew.js
@@ -0,0 +1,24 @@
+
+
+function mostrarCaja() {
+    console.log("TICKET DE COMPRA: ");
+
+    for (let i = 0; i < listaPedidos.length; i++) {
+        let elemento = listaPedidos[i];
+        
+        const { producto, precio } = elemento; 
+        
+        console.log(producto + " - $" + precio);
+    }
+
+    let subtotal = listaPedidos.reduce(function (acumulador, elemento) {
+        return acumulador + elemento.precio;
+    }, 0);
+
+    let iva = subtotal * 0.16;
+    let total = subtotal + iva;
+
+    console.log("Subtotal: $" + subtotal);
+    console.log("IVA: $" + iva);
+    console.log("Total: $" + total);
+}
